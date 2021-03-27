eec33ce1c0d1b40be8f25eeccb465c81c8d89623
PiKesi <73263112+PiKesi522@users.noreply.github.com>
2021-03-27 13:05:39 +0800

    Add files via upload

---
+++ 1035.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <math.h>
+
+using namespace std;
+
+class S{
+    public:
+        int r;
+    private:
+        int side_s;
+        int side_b;
+};
+
+int main()
+{
+
+}
